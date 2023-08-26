91cc14a2c3197703b70607b5b0faf9f716b722b6
pydlv <pydlv64@gmail.com>
2023-08-26 14:34:41 -0700

    function series

--- src/chat.ts
+++ src/chat.ts
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 import {Context, State} from "./state";
 import {writeJson} from "./utils";
-import {ALL_FUNCTIONS, OPEN_AI_FUNCTION_LIST} from "./functions";
+import {ALL_FUNCTIONS, ChatFunctionParams, OPEN_AI_FUNCTION_LIST} from "./functions";
 
 const prompt = require("prompt-sync")({
     sigint: true
@@ -12,19 +12,33 @@ interface UserInputTrigger {
     userMessage: string
 }
 
-type Trigger = UserInputTrigger;
+interface ResultTrigger {
+    type: "result",
+    call: FunctionSeriesItem,
+    result: string
+}
+
+type Trigger = UserInputTrigger | ResultTrigger;
 
 interface SystemMessageVariables {
-    trigger: Trigger
+    trigger_history: Trigger[]
     context: Context
 }
+
+interface FunctionSeriesItem {
+    function: string
+    purpose: string
+    arguments: ChatFunctionParams
+}
+
+type FunctionSeries = FunctionSeriesItem[]
 
 // Read contents of system_prompt.txt and store in a variable
 const SYSTEM_PROMPT = require("fs").readFileSync("./src/system_prompt.txt", "utf-8");
 
-export async function handleTrigger(client: OpenAI, state: State, trigger: Trigger) {
+export async function handleTrigger(client: OpenAI, state: State, triggerHistory: Trigger[]) {
     const systemMessageVariables: SystemMessageVariables = {
-        trigger,
+        trigger_history: triggerHistory,
         context: state.context
     };
 
@@ -41,33 +55,47 @@ export async function handleTrigger(client: OpenAI, state: State, trigger: Trigg
                 }, null, 0),
             },
         ],
-        function_call: "auto",
+        function_call: "none",
         model: "gpt-4"
     });
 
     const choice = completion.choices[0];
-    const functionCall = choice.message.function_call;
-    if (functionCall === undefined) {
-        console.log("No function call made:", choice);
+
+    if (choice.message.content === null) {
+        console.warn("Assistant did not return any message content.");
         return;
     }
 
-    const functionName = functionCall.name;
-    const func = ALL_FUNCTIONS.find(func => func.name === functionName);
-    if (func === undefined) {
-        console.warn("Assistant tried to execute unknown function:", functionName);
-        return;
+    const plannedFunctions: FunctionSeries = JSON.parse(choice.message.content);
+    if (plannedFunctions.length > 0) {
+        const nextFunction = plannedFunctions[0];
+
+        let funcName = nextFunction.function;
+        if (funcName.startsWith("functions.")) {
+            funcName = funcName.substring("functions.".length);
+        }
+
+        const func = ALL_FUNCTIONS.find(func => func.name === funcName);
+        if (func === undefined) {
+            console.warn("Assistant called unknown function:", funcName);
+            return;
+        }
+
+        const result = func.fn(state, nextFunction.arguments);
+
+        writeJson("state.json", state);
+
+        const newTriggerHistory: Trigger[] = [
+            ...triggerHistory,
+            {
+                type: "result",
+                call: nextFunction,
+                result: result ?? ""
+            }
+        ];
+
+        await handleTrigger(client, state, newTriggerHistory);
     }
-
-    const args = JSON.parse(functionCall.arguments);
-
-    if (functionName !== "send_message") {
-        console.log("Executing function:", functionName, "with args:", args);
-    }
-
-    func.fn(state, args);
-
-    writeJson("state.json", state);
 }
 
 export async function chatLoop(client: OpenAI, state: State) {
@@ -78,9 +106,9 @@ export async function chatLoop(client: OpenAI, state: State) {
             break;
         }
 
-        await handleTrigger(client, state, {
+        await handleTrigger(client, state, [{
             type: "user_input",
             userMessage: userInput
-        })
+        }])
     }
 }
